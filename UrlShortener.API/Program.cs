b7ce2bb7a9c25af2b7adda86e003ab30a8cd35db
Augusto-LJ <augustolimajardim@gmail.com>
2025-12-22 22:18:27 -0300

    Revert "feat: adds migrations only in development environment"
    This reverts commit 994271ef74410d016dab2bf32ec2eef603e64c9d.

--- UrlShortener.API/Program.cs
+++ UrlShortener.API/Program.cs
@@ -45,14 +45,17 @@ if (!string.IsNullOrEmpty(port))
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-
-    using var scope = app.Services.CreateScope();
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate();
 }
 
 app.UseRouting();
